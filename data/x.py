cae4de71c23ef16934564a7ea2c15cbd3e4c6732
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-10-09 23:27:58 -0300

    Update x.py

--- data/x.py
+++ data/x.py
@@ -1,14 +1,1 @@
-import re
-def a(response):
-    word = ""
-    for i, w in enumerate(response.split(".")):
-        if "-" in w:
-            if word != "":
-                word += ', ' + w.split("-")[1]
-            else:
-                print(w.split('-'))
 
-                word += w.split("-")[1]
-    return word
-
-print(a("Code smells: - Long method- Indirect access to variable - jos - dsa"))
